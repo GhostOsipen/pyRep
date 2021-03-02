155f264a5d805f636d5afcb927dd54e313f33d70
GhostOsipen <59918711+GhostOsipen@users.noreply.github.com>
2021-03-02 09:37:34 +0300

    Add files via upload

---
+++ PlusOne.py
@@ -0,0 +1,34 @@
+# Given a non-empty array of decimal digits representing a non-negative integer,
+# increment one to the integer.The digits are stored such that 
+# the most significant digit is at the head of the list, and each element
+# in the array contains a single digit.
+# You may assume the integer does not contain any leading zero, 
+# except the number 0 itself.
+
+def plusOne(digits: list) -> list:
+    temp = digits[::-1]
+    count = 0
+
+    for i in temp:
+        if i == 9:
+            count += 1
+        else:
+            break
+
+    if count != len(temp):
+        temp[0] += 1
+        for i in range(len(temp)):
+            if temp[i] > 9:
+                temp[i] = 0
+                temp[i + 1] += 1
+    else:
+        for i in range(len(temp)):
+            temp[i] = 0
+        temp.append(1)
+
+    return temp[::-1]
+
+print(plusOne([1,1,5,9]))
+print(plusOne([1,1,1,9]))
+print(plusOne([1,9,9,9]))
+print(plusOne([9,9,9,9]))
