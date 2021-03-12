9f86ca58cc37a0fe7400a6f891dd0d6fd517eeb6
Max Osipov <59918711+GhostOsipen@users.noreply.github.com>
2021-03-12 15:23:53 +0300

    add unfinished roman to int

---
+++ RomanToInteger.py
@@ -0,0 +1,30 @@
+# Symbol       Value
+# I             1
+# V             5
+# X             10
+# L             50
+# C             100
+# D             500
+# M             1000
+
+def roman_to_int(s: str) -> int:
+    answer = 0
+
+    table = {'I': 1,
+             'V': 5,
+             'X': 10,
+             'L': 50,
+             'C': 100,
+             'D': 500,
+             'M': 1000}
+
+    for i in reversed(range(len(s))):
+        answer += table[s[i]]
+       # if s[i+1] == 'V':
+           # print('qwerty')
+
+        
+    return answer
+
+
+print(roman_to_int('IV'))
