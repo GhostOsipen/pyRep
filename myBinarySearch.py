0a0a1bd16f062299051f7de8890b1fc77593a0a8
Max Osipov <59918711+GhostOsipen@users.noreply.github.com>
2021-03-15 16:43:41 +0300

    attempt 2 fix

--- myBinarySearch.py
+++ myBinarySearch.py
@@ -2,18 +2,22 @@ def my_binary_search(nums: list, target: int) -> int:
     nums.sort()
     maxN = len(nums) - 1
     minN = 0
+    mid = (maxN + minN) // 2
 
     if target < nums[minN] or target > nums[maxN]:
         return None
     else:
-        while nums[(maxN + minN) // 2] != target:
+        while nums[mid] != target:
             if (maxN-minN) == 1:
                 return None
             else:
-                if nums[(maxN + minN) // 2] > target:
-                    maxN = (maxN + minN) // 2
-                elif nums[(maxN + minN) // 2] < target:
-                    minN = (maxN + minN) // 2
+                if nums[mid] > target:
+                    maxN = mid
+                elif nums[mid] < target:
+                    minN = mid
+                mid = (maxN + minN) // 2
         else:
             print('Number of elements %s' % len(nums))
-            return (maxN + minN) // 2
+            return mid
+
+print(my_binary_search([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], 3))
