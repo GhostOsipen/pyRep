bbe87ca937b382839b3d60344a5bec99bf41d969
Max Osipov <59918711+GhostOsipen@users.noreply.github.com>
2021-03-16 12:47:01 +0300

    binary search ver3

--- myBinarySearch.py
+++ myBinarySearch.py
@@ -1,21 +1,16 @@
 def my_binary_search(nums: list, target: int) -> int:
-    nums.sort()
-    maxN = len(nums) - 1
-    minN = 0
-    mid = maxN
+    high = len(nums) - 1
+    low = 0
+    mid = high
 
-    if target < nums[minN] or target > nums[maxN]:
-        return None
-    else:
-        while maxN >= minN:
-            if target != nums[mid]:
-                mid = (maxN + minN) // 2
-                if target  <  nums[mid]:
-                    maxN = mid - 1
-                elif target  >  nums[mid]:
-                    minN = mid + 1
-            else:
-                print('Number of elements %s' % len(nums))
-                return mid
+    while high >= low:
+        if target != nums[mid]:
+            mid = (high + low) // 2
+            if target  <  nums[mid]:
+                high = mid - 1
+            elif target  >  nums[mid]:
+                low = mid + 1
         else:
-            return None
+            return mid
+    else:
+        return None
