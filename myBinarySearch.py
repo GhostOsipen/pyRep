a622fc9488801e91ec8a6f26afb5ee2a8c053cb1
Max Osipov <59918711+GhostOsipen@users.noreply.github.com>
2021-03-16 13:16:46 +0300

    programming is hard

--- myBinarySearch.py
+++ myBinarySearch.py
@@ -1,11 +1,12 @@
 def my_binary_search(nums: list, target: int) -> int:
     high = len(nums) - 1
     low = 0
-    mid = high
 
+    if target < nums[0] or target > nums[-1]:
+        return None
     while high >= low:
+        mid = (high + low) // 2
         if target != nums[mid]:
-            mid = (high + low) // 2
             if target  <  nums[mid]:
                 high = mid - 1
             elif target  >  nums[mid]:
