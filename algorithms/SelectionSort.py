120d0d365a66b0aa4200e4102189ba948686f993
Max Osipov <59918711+GhostOsipen@users.noreply.github.com>
2021-03-17 13:09:52 +0300

    selection sort try

---
+++ algorithms/SelectionSort.py
@@ -0,0 +1,17 @@
+def smallest_element(nums: list) -> int:
+    minN = nums[0]
+
+    for i in range(len(nums)):
+        if nums[i] < minN:
+            minN = nums[i]
+
+    return minN
+
+def my_selection_sort(nums: list) -> list:
+    sortedNums = []
+
+    while len(nums) > 0:
+        sortedNums.append(smallest_element(nums))
+        nums.pop(nums.index(smallest_element(nums)))
+
+    return sortedNums            
