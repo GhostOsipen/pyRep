d95aa6a86f34f11cac3bb309976d1b4d70973c03
Max Osipov <59918711+GhostOsipen@users.noreply.github.com>
2021-04-01 16:41:41 +0300

    01.04.21

---
+++ stepik/test1.py
@@ -0,0 +1,104 @@
+#=======================
+# x = int(input())
+# h = 60 * int(input())
+# m = int(input())
+# print(((h+m) + x)//60)
+# print(((h+m) + x)%60)
+#=======================
+# a = int(input())
+# b = int(input())
+# h = int(input())
+
+# if a <= h <= b:
+#     print("Это нормально")
+# elif h < a:
+#     print("Недосып")
+# elif h > b:
+#     print("Пересып")
+#======================
+# year = int(input())
+# if (year % 4) == 0:
+#     if (year % 100) == 0:
+#         if (year % 400) == 0:
+#             print("Високосный")
+#         else: 
+#             print("Обычный")    
+#     else:
+#         print("Високосный")       
+# else:
+#     print("Обычный")
+#======================
+# import math
+# a = int(input())
+# b = int(input())  
+# c = int(input())
+
+# p = (a+b+c)/2
+# s = math.sqrt(p*(p-a)*(p-b)*(p-c))
+# print(s)
+#======================
+# a = int(input())
+# if -15 < a <= 12 or 14 < a < 17 or a >= 19:
+#     print(True)
+# else:
+#     print(False)
+#======================
+# a = float(input())
+# b = float(input())  
+# o = input()
+
+# if o == "+":
+#     print(a+b)
+# elif o == "-":
+#     print(a-b)
+# elif o == "/":
+#     if b == 0:
+#         print("Деление на 0!")
+#     else:
+#         print(a/b)
+# elif o == "*":
+#     print(a*b)
+# elif o == "mod":
+#     if b == 0:
+#         print("Деление на 0!")
+#     else:
+#         print(a%b)
+# elif o == "pow":
+#     print(a**b)
+# elif o == "div":
+#     if b == 0:
+#         print("Деление на 0!")
+#     else:
+#         print(a//b)
+#======================
+# import math
+# t = input()
+
+# if t == "треугольник":
+#     a = int(input())
+#     b = int(input())  
+#     c = int(input())
+#     p = (a+b+c)/2
+#     print(math.sqrt(p*(p-a)*(p-b)*(p-c)))
+# elif t == "прямоугольник":
+#     a = int(input())
+#     b = int(input())
+#     print(a*b)
+# elif t == "круг":
+#     r = int(input())
+#     print(3.14 * r ** 2)
+#======================
+# a = int(input())
+# b = int(input())  
+# c = int(input())
+
+# nums = [a,b,c]
+# print(max(nums))
+# print(min(nums))
+# nums.remove(max(nums))
+# nums.remove(min(nums))
+# other = nums[0]
+# print(other)
+#====================== 
+
+#====================== 
