5780a196b58d675853e0fa9ea2c46e8206b13f20
Max Osipov <59918711+GhostOsipen@users.noreply.github.com>
2021-05-06 16:44:47 +0300

    phone book console

--- byte_of_python/myPhoneBook.py
+++ byte_of_python/myPhoneBook.py
@@ -7,19 +7,60 @@ class Person:
         self.phone = phone
         Person.d[name] = phone
     
-    @classmethod
-    def remove_person(cls, name):
-        Person.d.pop(name, None)
+    def __del__(self):
+        Person.d.pop(self.name, None)
 
+    # @classmethod
+    # def remove_person(cls, name):
+    #     Person.d.pop(name, None)
+
+    def change_person_name(self, newName):
+        Person.d[newName] = Person.d[self.name]
+        del Person.d[self.name]
+        self.name = newName
+    
+    def change_person_phone(self, newPhone):
+        Person.d[self.name] = newPhone
          
     @classmethod
     def data(cls):
         for k in Person.d.keys():
             print("Name: {0}, Phone: {1}".format(k, Person.d[k]))
 
+    @classmethod
+    def find_by_name(cls, s):
+        for k in Person.d.keys():
+            if s in k:
+                print("Name: {0}, Phone: {1}".format(k, Person.d[k]))
+
+    @classmethod
+    def find_by_phone(cls, s):
+        for k in Person.d.keys():
+            if str(s) in str(Person.d[k]):
+                print("Name: {0}, Phone: {1}".format(k, Person.d[k]))
+
+
+
 p = Person("Max", 123)
 p1 = Person("Eugene", 321)
-p.data()
-Person.remove_person("Max")
+Person.data()
+
+del p1
 print()
 Person.data()
+
+p.change_person_name("Mox")
+print()
+Person.data()
+
+p.change_person_phone(111)
+print()
+Person.data()
+
+p1 = Person("Eugene", 321)
+print()
+print("find")
+Person.find_by_name("Mo")
+Person.find_by_phone(1)
+
+#===================
