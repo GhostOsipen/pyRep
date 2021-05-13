344f259116c36c02cba2567005a64d2d168260cc
Max Osipov <59918711+GhostOsipen@users.noreply.github.com>
2021-05-13 15:39:11 +0300

    my phone book console ver. 13.05.21

--- byte_of_python/myPhoneBook.py
+++ byte_of_python/myPhoneBook.py
@@ -1,22 +1,28 @@
-
+import pickle
 
 class Person:
     d = {}
-    def __init__(self, name: str, phone: int):
-        self.name = name
-        self.phone = phone
-        Person.d[name] = phone
-    
-    def __del__(self):
-        Person.d.pop(self.name, None)
+   
+    @classmethod
+    def delete_person_by_name(cls, name: str):
+        del Person.d[name]
+        print(name, "deleted")
 
-    def change_person_name(self, newName):
-        Person.d[newName] = Person.d[self.name]
-        del Person.d[self.name]
-        self.name = newName
-    
-    def change_person_phone(self, newPhone):
-        Person.d[self.name] = newPhone
+    @classmethod
+    def add_person(cls, name: str, phone: int):
+        Person.d[name] = phone
+
+    @classmethod
+    def change_person_name(cls, oldName, newName):
+        Person.d[newName] = Person.d[oldName]
+        del Person.d[oldName]
+        oldName = newName
+        print("changed")
+
+    @classmethod
+    def change_person_phone(cls, name, newPhone):
+        Person.d[name] = newPhone
+        print("changed")
          
     @classmethod
     def data(cls):
@@ -35,74 +41,62 @@ class Person:
             if str(s) in str(Person.d[k]):
                 print("Name: {0}, Phone: {1}".format(k, Person.d[k]))
 
+while True:
+    s = input()
+    if s == "exit":
+        break
+    if s == "table":
+        Person.data()
+    if s == "add":
+        print("input name =>")
+        name = input()
+        print("input phone =>")
+        phone = input()
+        Person.add_person(name, phone)
+    if s == "delete":
+        print("input name =>")
+        Person.delete_person_by_name(name = input())
+    if s == "change":
+        print("what do you wanna change? =>")
+        change = input()
+        if change == "name":
+            print("old name =>")
+            old_name = input()
+            print("new name =>")
+            new_name = input()
+            Person.change_person_name(old_name, new_name)
+        elif change == "phone":  
+            print("name =>")
+            name = input()
+            print("new phone =>")
+            new_phone = input()
+            Person.change_person_phone(name, new_phone)
+    if s == "find":
+        print("find by name or phone? =>")
+        find = input()
+        if find == "name":
+            print("input name =>")
+            Person.find_by_name(s = input())
+        if find == "phone":
+            print("input phone = >")
+            Person.find_by_phone(s = input())
 
+    if s == "save":
+        print("save to data.pickle...")
+        with open('data.pickle', 'wb') as f:
+            pickle.dump(Person.d, f)
+    if s == "load":
+        print("load from data.pickle...")
+        with open('data.pickle', 'rb') as f:
+            Person.d = pickle.load(f)
 
-p = Person("Max", 123)
-p1 = Person("Eugene", 321)
-Person.data()
-
-del p1
-print()
-Person.data()
-
-p.change_person_name("Mox")
-print()
-Person.data()
-
-p.change_person_phone(111)
-print()
-Person.data()
-
-p1 = Person("Eugene", 321)
-print()
-print("find")
-Person.find_by_name("Mo")
-Person.find_by_phone(1)
-
-#===================
-import toga
-
-
-def add_handler(sender):
-    print("hello")
-
-def del_handler(widget):
-    print("hello")
-
-def build(app):
-    box = toga.Box()
-
-    add_person_cmd = toga.Command(
-        add_handler,
-        label="Add person",
-        tooltip="Add person tooltip")
-
-
-
-    table = toga.Table(['Name', 'Phone'])
-    table.style.padding = 10
-    table.style.flex = 1
-
-    # add_button = toga.Button('Hello world', on_press=add_button_handler)
-    # add_button.style.padding = 10
-    # add_button.style.flex = 1
-
-    # del_button = toga.Button('Hello world', on_press=del_button_handler)
-    # del_button.style.padding = 10
-    # del_button.style.flex = 1
-
-    # box.add(add_button)
-    # box.add(del_button)
-    box.add(table)
-
-    app.main_window.toolbar.add(add_person_cmd)
-
-    return box
-
-
-def main():
-    return toga.App('Phone Book', '1', startup=build)
-
-
-if __name__ == '__main__':
-    main().main_loop()
+    if s == "help":
+        print('''exit   <= to exit
+table  <= data list
+add    <= add person
+delete <= remove person
+change <= change person
+find   <= find someone
+save   <= to data.pickle
+load   <= load from data.pickle
+help   <= for help!''')
